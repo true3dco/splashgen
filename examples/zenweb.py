1e8269cecb87bca8a86999cde1113776146f2708
Travis Kaufman <travis.kaufman@gmail.com>
2021-05-17 17:45:58 -0400

    Remove extemporaneous comment from example file

--- examples/zenweb.py
+++ examples/zenweb.py
@@ -12,7 +12,6 @@ turn it into a web app that anyone on your team can use.
 Stop getting pinged every time an on-call engineer needs a script run,
 and start automating your domain expertise.
 """
-# This could probably be made easier
 site.meta = MetaTags(title=site.headline,
                      description="Automate your domain expertise. Sign up to join our pilot program!",
                      image="https://t3dmedia.s3.amazonaws.com/_notvideos/zwbg.png",
