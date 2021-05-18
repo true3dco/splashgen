1b414b7fe470157ae1448213ce96f96d76d822a9
Daniel Habib <dghabib7@gmail.com>
2021-05-18 10:59:21 -0700

    Update Splashgen Site

--- examples/splashgen_site.py
+++ examples/splashgen_site.py
@@ -2,14 +2,11 @@ from os import path
 from splashgen import MetaTags, SplashSite, launch
 from splashgen.integrations import MailchimpSignup
 
-site = SplashSite(title="Splashgen – Splash Pages Built In Python",
-                  logo=path.join(path.dirname(__file__), "logo.png"),
+site = SplashSite(title="Splashgen - Splash Pages Built In Python",
                   theme="dark")
-site.headline = "Effortlessly build your splash page in python"
-site.subtext = """
-In less than 30 lines of python, create beautiful and clean splash pages with Splashgen.
-Don''t play around with no code tools when you already know how to code. 
-"""
+site.headline = "Build your splash page in python effortlessly"
+site.subtext = """In less than 30 lines of python, create clean and beautiful splash pages with Splashgen.
+Don't waste time with no-code tools when you already know how to code."""
 site.meta = MetaTags(title=site.headline,
                      description="Build you splash page in Python. Sign up to join our pilot program!",
                      image="https://t3dmedia.s3.amazonaws.com/_notvideos/zwbg.png",
@@ -18,3 +15,4 @@ site.call_to_action = MailchimpSignup(
     "http://eepurl.com/hw4od9", button_text="Join our pilot")
 
 launch(site)
+
