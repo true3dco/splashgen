aa1212e212e256ed7155e35082f529e173a7b588
Daniel Habib <dghabib7@gmail.com>
2021-05-28 13:18:14 -0700

    Inherit from template

--- splashgen/templates/pricing_page.py
+++ splashgen/templates/pricing_page.py
@@ -1,50 +1,17 @@
-from splashgen import Component, MetaTags
-
-from os import path
-
-_ASSET_DIR = path.join(path.dirname(__file__), '../assets')
+from .base import Template
 
 
-class PricingPage(Component):
-    meta: MetaTags
+class PricingPage(Template):
 
     headline: str
     subtext: str
-    
-    # Only supports 2 pricing cards for now
-    pricing_cards = None# : list[PricingCard]
+    pricing_cards = None # : list[PricingCard]
 
+    template = "pricing_page.html.jinja"
 
-    def __init__(self, title: str = "Pricing Page 2", logo: str = None, meta: MetaTags = None, theme: str = "light") -> str:
-        super().__init__()
-        self.title = title
-        if not logo:
-            logo = path.join(_ASSET_DIR, "logo-default.png")
-        
-        self.logo = logo
-        self.meta = meta
-        if theme not in ["light", "dark"]:
-            raise ValueError(
-                "Invalid theme option. Please specify 'light' or 'dark'")
-        self.theme = theme
-        self.favicon_img = self.logo
+    def __init__(self, **kwargs) -> str:
+        super().__init__(**kwargs)
 
-        # Splash site 2 fields
         self.headline = "Fill out your headline here by assigning to `headline`"
         self.subtext = "Fill out subtext by assigning to `subtext`"
-
         self.pricingCards = None
-
-        self.enable_splashgen_analytics = True
-
-
-    def render(self) -> str:
-        logo_url = self.write_asset_to_build(self.logo)
-
-        favicons = self._gen_favicons()
-        return self.into_template("pricing_page.html.jinja", extras={
-            "logo": logo_url,
-            "favicons": favicons,
-        })
-
-
